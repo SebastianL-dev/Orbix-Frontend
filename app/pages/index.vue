ce031992273b18ebb7b95ad58ed1dab6bcc55e19
Sebastián Lozano <sebastianl2.dev@gmail.com>
2026-03-14 22:14:29 -0500

    feat: Add missions page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,14 +1,5 @@
-<script setup lang="ts">
-import Sidebar from "~/components/ui/sidebar.vue";
-import SpaceParticles from "~/components/particles/space-particles.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main class="relative flex w-full h-screen">
-    <SpaceParticles />
-
-    <Sidebar />
-
-    <div class="flex-1"></div>
-  </main>
+  <div class="flex-1"></div>
 </template>
